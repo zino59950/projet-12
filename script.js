a05287127ac361ce76de8f9b779c6ce09ee25070
zinedine <zinedinedref20@gmail.com>
2024-02-01 17:28:37 +0100

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function toggleMenu() {
-    var menuList = document.getElementById("menuList");
-    menuList.classList.toggle("show-menu");
+  var content = document.querySelector(".content");
+  content.classList.toggle("show-content");
 }
+
+function toggleCollapsible(index) {
+    var content = document.querySelectorAll('.content');
+
+    for (var i = 0; i < content.length; i++) {
+        if (i === index) {
+            content[i].style.maxHeight = content[i].style.maxHeight === '0px' ? content[i].scrollHeight + 'px' : '0';
+        } else {
+            content[i].style.maxHeight = '0';
+        }
+    }
+}
+
+// Ouvrir les deux collapsibles par défaut
+toggleCollapsible(0);
+toggleCollapsible(1);
