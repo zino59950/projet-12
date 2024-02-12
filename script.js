0f8ce1f77cd17981c1b0afbe892893d702dd7505
Zinedine Dref <zinediinedref20@gmail.com>
2024-02-12 19:14:26 +0100

    finish

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function toggleMenu() {
   var menuList = document.getElementById("menuList");
-  menuList.classList.toggle("show-menu"); // Ajoutez une classe CSS pour afficher/cacher le menu selon vos besoins
+  menuList.classList.toggle("show-menu");
 }
